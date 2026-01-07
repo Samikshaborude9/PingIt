23f0662cf1893652c38ba8ce76b2e33eccf257c1
Samiksha Borude <samikshaborude9@gmail.com>
2026-01-07 12:42:08 +0530

    feat: register route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,23 +1,57 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import User from "@/models/User";
-import { connectDB } from "@/lib/db"
+import { connectDB } from "@/lib/db";
 
-export async function POST(req: Request){
-    await connectDB();
-    const { email, password } = await req.json();
+export async function POST(req: Request) {
+  await connectDB();
 
-    const existing = await User.findOne({email});
-    if(existing){
-        return NextResponse.json({
-            message: "User already exist"
-        },
-    {
-        status: 400
-    })
-    }
-    const hashed = await bcrypt.hash(password, 10);
-    await User.create({email, password: hashed});
+  const {
+    name,
+    email,
+    phone,
+    password,
+    password_confirmation,
+  } = await req.json();
 
-    return NextResponse.json({ message: "Registered successfully"});
+  //backend validation
+  if (!email || !password || !name) {
+    return NextResponse.json(
+      { message: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+  //Password confirmation
+  if (password !== password_confirmation) {
+    return NextResponse.json(
+      { message: "Passwords do not match" },
+      { status: 400 }
+    );
+  }
+
+  //Check existing user
+  const existing = await User.findOne({ email });
+  if (existing) {
+    return NextResponse.json(
+      { message: "User already exists" },
+      { status: 400 }
+    );
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //Create user
+  await User.create({
+    name,
+    email,
+    phone,
+    password: hashedPassword,
+    isVerified: false,
+  });
+
+  return NextResponse.json({
+    message: "Registered successfully. Please verify your email.",
+  });
 }
--- models/User.ts
+++ models/User.ts
@@ -1,11 +1,39 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    isVarified: {type: Boolean, default: false},
-    createdAt: {type: Date, default: Date.now },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User ||
+  mongoose.model("User", UserSchema);
